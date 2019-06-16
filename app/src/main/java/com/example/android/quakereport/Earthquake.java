8d2905abaf4c1356194e37e054c3a78abe6d9785
Anuj Kumar Nath <anujkumarnath@gmail.com>
2019-06-16 20:25:42 +0530

    Added Earthquake data model

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,25 @@
+package com.example.android.quakereport;
+
+public class Earthquake {
+    private String mMagnitude;
+    private String mLocality;
+    private String mDate;
+
+    public void Earthquake(String magnitude, String locality, String date) {
+        mMagnitude = magnitude;
+        mLocality = locality;
+        mDate = date;
+    }
+
+    public String getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getmLocality() {
+        return mLocality;
+    }
+
+    public String getDate() {
+        return mDate;
+    }
+}
